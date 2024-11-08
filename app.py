31a9f150a3333cc4530ab352134ed97dd5a554e7
hris98 <hris6691@gmail.com>
2024-11-08 17:52:29 -0500

    Added a few functions for the movie console app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+import csv
 
 class Movie:
     def __init__(self,name,year,director):
@@ -6,10 +8,24 @@ class Movie:
         self.director = director
 
 def getAll():
-    print("All movies")
+    if os.path.exists("data.csv"):
+        with open("data.csv","r") as filmsFile:
+            reader = csv.DictReader(filmsFile,)
+            l = list(reader)
+            movies = {m["name"] : m for m in l }
+            for movieId in movies:
+               print(movies[movieId])
+                
 
-def getByName():
-    print("Movie by name")
+def getByName(name):
+    if os.path.exists("data.csv"):
+        with open("data.csv","r") as filmsFile:
+            reader = csv.DictReader(filmsFile,)
+            l = list(reader)
+            movies = {m["name"] : m for m in l }
+            if name in movies:
+             print(movies[name])
+            else: print("No results")
 
 def getByYear():
     print("By year")
@@ -17,8 +33,39 @@ def getByYear():
 def getByDirector():
     print("By director")
 
+def updateMovies(movies):
+    fields = ['name','year','director']
+    with open ("data.csv","w", newline='') as movieFile:
+        writer = csv.writer(movieFile)
+        writer.writerow(fields)
+        for movieID in movies:
+            movie = movies[movieID]
+            writer.writerow([movie.name, movie.year , movie.director])
 
 
-print(" 1: List all Movies\n","2: Search movie by name\n","3: Search movie by release date\n","4: Search movie by director")
+movies = {
+   "The Exorcist": Movie("The Exorcist","1973","William Friedkin"),
+   "Hereditary": Movie("Hereditary","2018","Ari Aster"),
+   "A Nightmare on Elm Street": Movie("A Nightmare on Elm Street","1984","Wes Craven"), 
+}
+
+print(" 1: List all Movies\n","2: Search movie by name\n","3: Search movie by release date\n","4: Search movie by director\n","5: To exit")
 choice = int(input())
-print(choice)
+
+match choice:
+    case 1:
+        getAll()
+
+    case 2:
+        name = input("please type a movie name: ")
+        getByName(name)
+
+    case 3:
+        getByYear()
+
+    case 4:
+        getByDirector()
+
+    case 5:
+        exit
+   
